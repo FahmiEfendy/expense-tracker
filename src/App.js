2bfeea916de9e9bc62487b5b90823797164986b3
Fahmi Efendy <itsfahmiefendy@gmail.com>
2022-09-20 13:15:21 +0700

    feat add ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <h2>Hello World!</h2>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
