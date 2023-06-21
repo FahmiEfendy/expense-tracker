f4c88659469910700e8e3c5a1c26e26b4bd8a0f0
Fahmi <itsfahmiefendy@gmail.com>
2023-06-21 11:33:00 +0700

    feat: add responsive for phone size

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -28,14 +28,26 @@ const ExpenseItem = (props) => {
           Rp{price.toLocaleString("id-ID")},00
         </div>
       </div>
-      <button className="icon-container edit-icon" onClick={editExpenseHandler}>
-        <box-icon name="pencil" type="solid" color="black"></box-icon>
+      <button className="icon-container" onClick={editExpenseHandler}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="edit-icon"
+          viewBox="0 0 24 24"
+        >
+          <path d="M8.707 19.707 18 10.414 13.586 6l-9.293 9.293a1.003 1.003 0 0 0-.263.464L3 21l5.242-1.03c.176-.044.337-.135.465-.263zM21 7.414a2 2 0 0 0 0-2.828L19.414 3a2 2 0 0 0-2.828 0L15 4.586 19.414 9 21 7.414z"></path>
+        </svg>
       </button>
       <button
-        className="icon-container delete-icon"
+        className="icon-container icon-container-delete"
         onClick={deleteExpenseHandler}
       >
-        <box-icon type="solid" name="trash" color="#FFFFFF"></box-icon>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="delete-icon"
+          viewBox="0 0 24 24"
+        >
+          <path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm10.618-3L15 2H9L7.382 4H3v2h18V4z"></path>
+        </svg>
       </button>
     </Card>
   );
